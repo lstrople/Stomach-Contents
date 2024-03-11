a264c0ea2eff9f33d6fbe8de0884f6f6166565e7
Leah <55734721+lstrople@users.noreply.github.com>
2024-03-11 11:38:03 +0100

    Update

--- Rinsp_specialisation.R
+++ Rinsp_specialisation.R
@@ -1,61 +1,12 @@
-##########
-#Library
-##########
-library(RInSp)
-library(vegan)
-################################
-#Discrete - stickleback example
-################################
-
-
-data(Stickleback)
-
-#Import dataset
-
-Site_A = import.RInSp(Stickleback, row.names = 1, info.cols = c(2:13),subset.rows = c("Site", "A"))
-
-
-WT = WTdMC(Site_A, replicates = 999)
-
-SiteA_E = Emc(Site_A, popd.type = "average", replicates = 999) #Evalue 
-
-Null.data = MCp.RInSp(Site_A, replicates = 999)
-
-
-null.d.NODF = c() 
-for (i in 1:(999 + 1)) {
-  null.d.NODF = c(null.d.NODF, NODF(import.RInSp(Null.data[ , , i], print.messages=FALSE), print.results=FALSE)$NODF)}
-
-null.d.NODF[1]
-
-
-summary(null.d.NODF)
-hist(null.d.NODF)
-abline(v=null.d.NODF[1])
-
-###########################
-#Continuous trout example
-##########################
-
-data(Trout)
-#import dataset
-TroutRIS = import.RInSp(Trout, col.header=TRUE, row.names=1, info.cols=c(1:6), data.type="double")
-#specialisation 0 is strong specialisation and 1 is generalisation
-results <- WTcMC(TroutRIS, replicates = 999)
-#weighted specialisation
-results= WTcMC(TroutRIS, weight="N_items", replicates=999)
-#E index 0 is all ind same resources in same proportion, 1 every ind relies on different resources
-TroutE = Eindex(TroutRIS, index = "saramaki", jackknife = TRUE)
-
-decomp = Hier2L(TroutRIS, factor=1)
-
-#histogram
-sumMC.RInSp(results)
 
 
 ###########
 #Chichocs
 ###########
+
+setwd("C:/Users/lstrople/OneDrive - Norwegian University of Life Sciences/Winter_paper/KML files")
+
+safo.df <- read.csv("C:/Users/lstrople/OneDrive - Norwegian University of Life Sciences/Mathias/stomach contents/stomachcontentsafo.csv")
 
 winter <- subset(safo, Season=="Winter")
 summer <- subset(safo, Season=="Summer")
@@ -65,7 +16,7 @@ summer <- subset(safo, Season=="Summer")
 ################
 
 #Haymard 
-safoHayRIS = import.RInSp(safo, col.header=TRUE, row.names=1, info.cols=c(1:6), data.type = "integer", subset.rows = c("Lake", "Haymard"))
+safoHayRIS = import.RInSp(stomachcontentsafo, col.header=TRUE, row.names=1, info.cols=c(1:6), data.type = "integer", subset.rows = c("Lake", "Haymard"))
 
 resultsHay <- WTcMC(safoHayRIS, replicates = 999)
 
