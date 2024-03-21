ddbb9cff5a164c9c38f325b7ac49c8ee37dbdcef
Leah <55734721+lstrople@users.noreply.github.com>
2024-03-21 10:57:20 +0100

    Create Empty_stomachs.R

---
+++ Empty_stomachs.R
@@ -0,0 +1,13 @@
+library(glm2)
+library(lm)
+
+empty.df <- read.csv("C:/Users/lstrople/OneDrive - Norwegian University of Life Sciences/Mathias & Leah/empty_stomachs.csv", header=TRUE)
+
+model1.glm <- glm(Empt1.1.0.~ Seaso0*Lake, family=binomial,
+                data=empty.df)
+
+summary(model1.glm)
+
+anova(model1.glm, test ='Chi')
+
+drop1(model1.glm, test = "Chi")
