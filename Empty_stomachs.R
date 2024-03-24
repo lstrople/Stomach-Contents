9f1f8b943df5166c7d1dbfee78d31589fc07bbe2
Leah <55734721+lstrople@users.noreply.github.com>
2024-03-24 12:28:43 +0100

    Update Empty_stomachs.R

--- Empty_stomachs.R
+++ Empty_stomachs.R
@@ -3,11 +3,11 @@ library(lm)
 
 empty.df <- read.csv("C:/Users/lstrople/OneDrive - Norwegian University of Life Sciences/Mathias & Leah/empty_stomachs.csv", header=TRUE)
 
-model1.glm <- glm(Empt1.1.0.~ Seaso0*Lake, family=binomial,
+model1.glm <- glm(Empt1.1.0.~ Seaso0, family=binomial,
                 data=empty.df)
 
 summary(model1.glm)
 
 anova(model1.glm, test ='Chi')
 
-drop1(model1.glm, test = "Chi")
+
